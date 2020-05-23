1e88e001c6e69d0127af783bd6b657e78d9d40d2
Temetra <temetra.xy+git@gmail.com>
2020-05-23 04:27:43 +0100

    Tooltip delay etc

--- src/components/tooltips/Tooltip.svelte
+++ src/components/tooltips/Tooltip.svelte
@@ -1,12 +1,39 @@
 <script>
 	import { afterUpdate } from "svelte";
-	import { showTooltip, hideTooltip } from "~/modules/tooltip.js";
 	export let store;
 	
-	let tooltipElement, position;
-	$: target = $store ? $store.target : null;
-	$: template = $store ? $store.template : null;
-	$: payload = $store ? $store.payload : null;
+	let tooltipElement, 
+		position,
+		target, 
+		template, 
+		payload,
+		timerRef;
+
+	// Tooltip is removed when store is cleared
+	// Tooltip element has mouse events to prevent clearing
+	$: {
+		if ($store) {
+			cancelReset();
+			({target, template, payload} = $store);
+		}
+		else if (target) {
+			scheduleReset();
+		}
+	}
+
+	// Allow 50ms for cursor to enter tooltip
+	function scheduleReset() {
+		timerRef = timerRef || setTimeout(() => {
+			timerRef = null;
+			target = template = payload = null;
+		}, 50);
+	}
+
+	// Cancel scheduled reset
+	function cancelReset() {
+		clearTimeout(timerRef);
+		timerRef = null;
+	}
 
 	// Move tooltip after content is mounted
 	afterUpdate(() => {
@@ -15,6 +42,36 @@
 			else hideTooltip(tooltipElement);
 		}
 	});
+
+	function showTooltip(targetElement, tooltipElement) {
+		// Get size of elements
+		let targetRect = targetElement.getBoundingClientRect();
+		let tooltipRect = tooltipElement.getBoundingClientRect();
+
+		// Compute tooltip position
+		let position = "bottom";
+		let top = targetRect.top + targetRect.height + window.scrollY;
+
+		// Check if content extends past page, reverse position if so
+		if (top + tooltipRect.height - window.scrollY >= window.innerHeight) {
+			position = "top";
+			top = targetRect.top - tooltipRect.height + window.scrollY;
+		}
+
+		// Show tooltip
+		tooltipElement.style.visibility = "visible";
+		tooltipElement.style.top = `${top}px`;
+		tooltipElement.style.left = `${targetRect.left}px`;
+
+		// Return calculated position
+		return position;
+	}
+
+	function hideTooltip(tooltipElement) {
+		tooltipElement.style.visibility = "hidden";
+		tooltipElement.style.top = "0px";
+		tooltipElement.style.left = "0px";
+	}
 </script>
 
 <style type="text/scss">
@@ -28,74 +85,53 @@
 	$foreground:white;
 
 	.tooltip {
+		z-index:500;
 		position:absolute;
-		z-index:1;
 		visibility:hidden;
+		display: grid;
+		grid-template-areas:
+			"arrow"
+			"content";
+	}
+
+	.tooltip.top {
+		grid-template-areas:
+			"content"
+			"arrow";
+	}
+
+	.arrow {
+		grid-area:arrow;
+		width:$pointer-width;
+		margin:(-$pointer-length) 0 0 1rem;
+		border:solid $pointer-width transparent;
+		border-bottom:solid $pointer-length $background;
+	}
+
+	.top .arrow {
+		margin:0 0 (-$pointer-length) 1rem;
+		border:solid $pointer-width transparent;
+		border-top:solid $pointer-length $background;
+	}
+
+	.content {
+		grid-area:content;
 		color:$foreground;
 		background:$background;
 		padding:0.75rem;
 		border-radius:0.25rem;
-
-		&::after {
-			content:" ";
-			position:absolute;
-			border:solid $pointer-width transparent;
-		}
-	}
-
-	.tooltip.top {
-		margin:(-$pointer-length) 0 0 0;
-
-		&::after {
-			top:100%;
-			left:50%;
-			margin-left:-$pointer-width;
-			border-top:solid $pointer-length $background;
-		}
-	}
-
-	.tooltip.bottom {
-		margin:$pointer-length 0 0 0;
-
-		&::after {
-			bottom:100%;
-			left:50%;
-			margin-left:(-$pointer-width);
-			border-bottom:solid $pointer-length $background;
-		}
-	}
-
-	.tooltip.right {
-		margin:0 0 0 $pointer-length;
-
-		&::after {
-			top:50%;
-			right:100%;
-			margin-top:-$pointer-width;
-			border-right:solid $pointer-length $background;
-		}
-	}
-
-	.tooltip.left {
-		margin:0 0 0 (-$pointer-length);
-
-		&::after {
-			top:50%;
-			left:100%;
-			margin-top:-$pointer-width;
-			border-left:solid $pointer-length $background;
-		}
 	}
 </style>
 
 <section>
-	<div class="tooltip" 
+	<div 
+		class="tooltip" 
 		class:top={position == "top"} 
-		class:bottom={position == "bottom"} 
-		class:left={position == "left"} 
-		class:right={position == "right"} 
 		bind:this={tooltipElement}
+		on:mouseenter={cancelReset}
+		on:mouseleave={scheduleReset}
 	>
-		<svelte:component this={template} {payload} />
+		<div class="arrow"></div>
+		<div class="content"><svelte:component this={template} {payload} /></div>
 	</div>
 </section>
