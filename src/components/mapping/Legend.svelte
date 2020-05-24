78e94295e2c1418675a1136f2ba163c855f0fd1a
Temetra <temetra.xy+git@gmail.com>
2020-05-24 09:58:33 +0100

    Simplified legend

--- src/components/mapping/Legend.svelte
+++ src/components/mapping/Legend.svelte
@@ -1,9 +1,18 @@
 <script>
 	import { mapIntervals, mapMode } from "~/stores/datastore.js";
 
+	$: title = ($mapMode == "raw") ? "Total cases in region" : "Cases per 10,000 people";
+
 	function toggleMapMode() {
-		if ($mapMode == "raw") $mapMode = "perPop";
-		else $mapMode = "raw";
+		$mapMode = ($mapMode == "raw") ? "perPop" : "raw";
+	}
+
+	function getLabel(values) {
+		if (values.unknown) return "Unknown";
+		else if (values.zero) return "Zero";
+		else if (values.to && !values.from) return `Less than ${values.to}`;
+		else if (!values.to && values.from) return `${values.from} or more`;
+		return `${values.from}-${values.to-1}`;
 	}
 </script>
 
@@ -16,39 +25,13 @@
 
 {#if $mapIntervals.size > 0}
 	<div class="title" on:click={toggleMapMode}>
-		{#if $mapMode == "perPop"}
-			Cases per 10,000 people
-		{:else}
-			Total cases in region
-		{/if}
+		{title}
 	</div>
 	{#each [...$mapIntervals] as {values, style}}
-		{#if values.unknown}
-			<div class="keyitem">
-				<div class="colour {style}"><div></div></div>
-				<div class="label">Unknown</div>
-			</div>
-		{:else if values.zero}
-			<div class="keyitem">
-				<div class="colour {style}"><div></div></div>
-				<div class="label">Zero</div>
-			</div>
-		{:else if values.to && !values.from}
-			<div class="keyitem">
-				<div class="colour {style}"><div></div></div>
-				<div class="label">Less than {values.to}</div>
-			</div>
-		{:else if !values.to && values.from}
-			<div class="keyitem">
-				<div class="colour {style}"><div></div></div>
-				<div class="label">{values.from} or more</div>
-			</div>
-		{:else}
-			<div class="keyitem">
-				<div class="colour {style}"><div></div></div>
-				<div class="label">{values.from}-{values.to-1}</div>
-			</div>
-		{/if}
+		<div class="keyitem">
+			<div class="colour {style}"><div></div></div>
+			<div class="label">{getLabel(values)}</div>
+		</div>
 	{/each}
 {:else}
 	Loading...
